e685a5b381ff538467d9ca27b51e0e50399c185d
gurustump <gurustump@gurustump.com>
2014-12-14 14:59:11 -0800

    fixing some layout issues for mobile

--- themes/christmasbarrel-fundify/js/christmasbarrel.js
+++ themes/christmasbarrel-fundify/js/christmasbarrel.js
@@ -14,4 +14,39 @@ jQuery( document ).ready(function( $ ) {
 			}, 2000).toggleClass('wpadminbar-shown');
 		}
 	).append('<div class="wpadminbar-activator"></div>');
+	
+	$(window).resize(function() {
+		if(this.resizeTO) {clearTimeout(this.resizeTO)}
+		this.resizeTO = setTimeout(function() {
+			$(this).trigger('resizeEnd')
+		}, 150)
+	})
+	
+	$(window).bind('resizeEnd',function() {
+		mobileCalendarLayout($)
+	})
+	mobileCalendarLayout($)
 });
+
+function mobileCalendarLayout($) {
+	if (mobileCheck(425)) {
+		$('.vid-nav > li > .advent-number').each(function() {
+			newHeight = Math.floor($(this).outerWidth() * .555)
+			$(this).height(newHeight).css({
+				'line-height':newHeight * 2 / 3 + 'px',
+				'font-size':newHeight / 2 + 'px'
+			})
+		})
+	} else {
+		$('.vid-nav > li > .advent-number').removeAttr('style')
+	}
+}
+
+
+function mobileCheck(maxWidth) {
+	if (jQuery(window).width() > maxWidth) {
+		return false
+	} else {
+		return true
+	}
+}
